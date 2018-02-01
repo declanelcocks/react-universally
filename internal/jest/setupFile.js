8dd571913f6c3890e81e0e4d89be60e06c3c5869
Declan Elcocks <declan@altitudelabs.com>
2018-02-01 10:20:12 +0800

    Add mockFetch for action test

--- internal/jest/setupFile.js
+++ internal/jest/setupFile.js
@@ -1,6 +1,23 @@
 /* eslint import/first: "off" */
-import '../../shared/polyfills';
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import '../../shared/polyfills'
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
-configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() })
+
+const mockResponse = (status, statusText, response) =>
+  new window.Response(JSON.stringify(response), {
+    status,
+    statusText,
+    headers: {
+      'Content-type': 'application/json',
+    },
+  })
+
+global.mockFetch = (status, statusText, response) => {
+  window.fetch = jest
+    .fn()
+    .mockImplementation(() =>
+      Promise.resolve(mockResponse(status, statusText, response)),
+    )
+}
