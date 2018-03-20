b35ffb2a7edcf99d10e2ec755d1d968496e19669
Declan Elcocks <declan@altitudelabs.com>
2018-03-20 12:26:32 +0800

    Add docs for service worker

--- client/registerServiceWorker.js
+++ client/registerServiceWorker.js
@@ -1,31 +1,28 @@
 /**
- * We use the offline-plugin to generate a service worker.  See the webpack
- * config for more details.
+ * `offline-plugin` is used to generate and register the SW. The SW config
+ * is handled inside `internal/webpack`.
  *
- * We need to ensure that the runtime is installed so that the generated
- * service worker is executed.
- *
- * NOTE: We only enable the service worker for non-development environments.
+ * NOTE: We only enable the SW for non-development environments.
  */
 
 import config from '../config'
 
 if (process.env.BUILD_FLAG_IS_DEV === 'false') {
-  // We check the shared config, ensuring that the service worker has been
-  // enabled.
+  // Check the SW has been enabled.
   if (config('serviceWorker.enabled')) {
     // eslint-disable-next-line global-require
+    // Ensure that the offline-plugin runtime is installed. This ensures
+    // that the SW can be executed.
     const OfflinePluginRuntime = require('offline-plugin/runtime')
 
-    // Install the offline plugin, which instantiates our service worker and app
-    // cache to support precaching of assets and offline support.
+    // Install & run offline-plugin.
     OfflinePluginRuntime.install({
       onUpdating: () => undefined,
-      // When an update is ready we will tell the new SW to take control immediately.
+      // When an update is ready we tell the new SW to update straight away.
       onUpdateReady: () => OfflinePluginRuntime.applyUpdate(),
-      // After the new SW update has been applied we will reload the users page
-      // to ensure they are using the latest assets.
-      // This only gets run if there were updates available for our cached assets.
+      // After the new SW update, reload the page to ensure the user has
+      // the latest assets. This only runs if there were updates available for
+      // the cached assets.
       onUpdated: () => window.location.reload(),
       onUpdateFailed: () => undefined,
     })
